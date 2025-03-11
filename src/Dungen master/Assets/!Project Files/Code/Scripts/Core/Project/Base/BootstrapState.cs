7c8de9dd707f035127597097f16da1d557660e79
ShutovKS <schutow.kir@yandex.ru>
2025-03-11 03:14:12 +0300

    Add project manager and bootstrap state

---
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Core/Project/Base/BootstrapState.cs
@@ -0,0 +1,32 @@
+﻿using Cysharp.Threading.Tasks;
+using Services;
+using StateMachines.DirectControlMultiLayer;
+using UnityEngine;
+
+namespace Core.Project
+{
+    public class BootstrapState : IState
+    {
+        public bool IsReusable => false;
+
+        private readonly IProjectEngine _stateMachine;
+
+        public BootstrapState(
+            IProjectEngine stateMachine
+        )
+        {
+            _stateMachine = stateMachine;
+        }
+
+        public UniTask Initialize()
+        {
+            // _stateMachine.ChangeState<OpenLoadingScreenState>();
+
+            return UniTask.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Core/Project/Base/ExitFromApplicationState.cs
@@ -0,0 +1,25 @@
+﻿using Cysharp.Threading.Tasks;
+using StateMachines.DirectControlMultiLayer;
+
+namespace Core.Project
+{
+    public class ExitFromApplicationState : IState
+    {
+        public bool IsReusable => true;
+
+        public UniTask Initialize()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            UnityEngine.Application.Quit();
+#endif
+
+            return UniTask.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
