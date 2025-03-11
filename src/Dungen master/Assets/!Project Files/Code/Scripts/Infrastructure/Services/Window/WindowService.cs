ae8621f8bbd64e39d50f91aa578ee76d771f9e20
ShutovKS <schutow.kir@yandex.ru>
2025-03-11 03:12:10 +0300

    Add UI factory and Windows servece

---
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Infrastructure/Services/Window/WindowService.cs
@@ -0,0 +1,64 @@
+﻿using Cysharp.Threading.Tasks;
+using Factories;
+using UnityEngine;
+
+namespace Services
+{
+    /// <summary>
+    /// Сервис для управления окнами в приложении.
+    /// </summary>
+    public class WindowService : IWindowService
+    {
+        public WindowService(
+            IUIFactory uiFactory
+        )
+        {
+            _uiFactory = uiFactory;
+        }
+
+        private readonly IUIFactory _uiFactory;
+
+        /// <inheritdoc/>
+        public bool IsWindowOpened(WindowID windowID) =>
+            _uiFactory.Exists(windowID);
+
+        /// <inheritdoc/>
+        public async UniTask Open(WindowID windowID)
+        {
+            var windowsPath = GetWindowsPath(windowID);
+
+            await _uiFactory.CreateScreen(windowsPath, windowID);
+        }
+
+        /// <inheritdoc/>
+        public async UniTask<T> OpenAndGet<T>(WindowID windowID) where T : Component
+        {
+            await Open(windowID);
+
+            return _uiFactory.GetScreenComponent<T>(windowID);
+        }
+
+        /// <inheritdoc/>
+        public T Get<T>(WindowID windowID) where T : Component =>
+            _uiFactory.GetScreenComponent<T>(windowID);
+
+        /// <inheritdoc/>
+        public void Close(WindowID windowID) =>
+            _uiFactory.DestroyScreen(windowID);
+
+        private static string GetWindowsPath(WindowID windowID)
+        {
+            return windowID switch
+            {
+                WindowID.GameLoading => WindowsPaths.GAME_LOADING_PATH,
+                WindowID.MainMenu => WindowsPaths.MAIN_MENU_PATH,
+                WindowID.Settings => WindowsPaths.SETTINGS_PATH,
+                WindowID.Game => WindowsPaths.GAME_PATH,
+                WindowID.PauseMenu => WindowsPaths.PAUSE_MENU_PATH,
+                WindowID.GameOver => WindowsPaths.GAME_OVER_PATH,
+                WindowID.Victory => WindowsPaths.VICTORY_PATH,
+                _ => null
+            };
+        }
+    }
+}
