8d514fad93430c2eef9d855786a782e401b0de82
ShutovKS <schutow.kir@yandex.ru>
2025-03-11 03:11:21 +0300

    Add UnityGameLoopObserver

---
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Infrastructure/Observers/UnityGameLoop/IUnityGameLoopObserver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Observers
+{
+    /// <summary>
+    /// Интерфейс для наблюдателей за игровым циклом Unity.
+    /// </summary>
+    public interface IUnityGameLoopObserver
+    {
+        /// <summary>
+        /// Событие, вызываемое при изменении фокуса приложения.
+        /// </summary>
+        public event Action<bool> ApplicationFocusChanged;
+
+        /// <summary>
+        /// Событие, вызываемое при изменении состояния паузы приложения.
+        /// </summary>
+        public event Action<bool> ApplicationPauseChanged;
+
+        /// <summary>
+        /// Событие, вызываемое при выходе из приложения.
+        /// </summary>
+        public event Action ApplicationQuit;
+
+        /// <summary>
+        /// Событие, вызываемое при обновлении приложения.
+        /// </summary>
+        public event Action ApplicationUpdate;
+    }
+}
