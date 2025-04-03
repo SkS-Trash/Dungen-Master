4cd47b20edddda8c35d1329f5f3c63714c84ba9c
ShutovKS <schutow.kir@yandex.ru>
2025-04-03 09:51:28 +0300

    Add spels

---
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Data/Magic/LightningSpell.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Magic
+{
+    [CreateAssetMenu(menuName = "Data/Spells/LightningSpell")]
+    public class LightningSpell : Spell
+    {
+        public float radius = 5f;
+        public GameObject lightningEffectPrefab;
+
+        public override void Cast(Transform castPoint)
+        {
+            // Для молнии можно, например, создать эффект в заданном радиусе
+            var effectInstance = Instantiate(lightningEffectPrefab, castPoint.position, Quaternion.identity);
+            // Реализуйте логику поиска врагов и нанесения урона в радиусе
+        }
+    }
+}
---
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Data/Magic/MagicBallSpell.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Magic
+{
+    [CreateAssetMenu(menuName = "Data/Spells/MagicBallSpell")]
+    public class MagicBallSpell : Spell
+    {
+        [SerializeField] private float speed = 1f;
+
+        public override async void Cast(Transform castPoint)
+        {
+            var position = castPoint.position;
+            var rotation = castPoint.rotation;
+            var forward = castPoint.forward;
+            
+            var spellInstance = await Addressables.InstantiateAsync(SpellPrefab).Task;
+            
+            spellInstance.transform.position = position;
+            spellInstance.transform.rotation = rotation;
+            
+            var rb = spellInstance.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.linearVelocity = forward * speed;
+            }
+
+            Destroy(spellInstance, 5f);
+        }
+    }
+}
