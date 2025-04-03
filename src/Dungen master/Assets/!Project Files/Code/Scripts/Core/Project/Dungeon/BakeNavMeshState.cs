7bd68df2f569849fdf0ebb2c28d4261121d77d27
ShutovKS <schutow.kir@yandex.ru>
2025-04-04 01:29:17 +0300

    Add BakeNavMeshState

---
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Core/Project/Dungeon/BakeNavMeshState.cs
@@ -0,0 +1,29 @@
+﻿using Cysharp.Threading.Tasks;
+using Factories.GameObject;
+using StateMachines.DirectControlMultiLayer.ForState;
+using Unity.AI.Navigation;
+using UnityEngine;
+
+namespace Core.Project.Dungeon
+{
+    public class BakeNavMeshState : IStateOneShot
+    {
+        private readonly IGameObjectFactory _gameObjectFactory;
+
+        public BakeNavMeshState(
+            IGameObjectFactory gameObjectFactory
+        )
+        {
+            _gameObjectFactory = gameObjectFactory;
+        }
+
+        public async UniTask OnEnterAsync(Unit _)
+        {
+            var parentObject = await _gameObjectFactory.InstantiateAsync(GameObjectsPaths.EMPTY_GAME_OBJECT);
+            parentObject.name = "NavMeshBake";
+            var navMeshSurface = parentObject.AddComponent<NavMeshSurface>();
+
+            navMeshSurface.BuildNavMesh();
+        }
+    }
+}
--- src/Dungen master/Assets/!Project Files/Code/Scripts/Data/Paths/GameObjectsPaths.cs
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Data/Paths/GameObjectsPaths.cs
@@ -1,4 +1,6 @@
 ﻿public static class GameObjectsPaths
 {
     public const string PLAYER = "Objects/Player";
+
+    public const string EMPTY_GAME_OBJECT = "EmptyGameObject";
 }
