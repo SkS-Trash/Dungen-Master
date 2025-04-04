0158770318f1f02b76ffd6d8a2efa2f9bc0ce61c
ShutovKS <schutow.kir@yandex.ru>
2025-04-04 10:02:25 +0300

    Add EnemyMovement

--- src/Dungen master/Assets/!Project Files/Code/Scripts/Features/Enemy/EnemyCore.cs
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Features/Enemy/EnemyCore.cs
@@ -2,26 +2,28 @@
 
 namespace Enemy
 {
-    [RequireComponent(typeof(EnemyHealth))]
+    [RequireComponent(typeof(EnemyHealth), typeof(EnemyMovement))]
     public class EnemyCore : MonoBehaviour
     {
         private EnemyHealth _enemyHealth;
-        
+        private EnemyMovement _enemyMovement;
+
         private void Awake()
         {
             _enemyHealth = GetComponent<EnemyHealth>();
+            _enemyMovement = GetComponent<EnemyMovement>();
         }
 
         private void OnEnable()
         {
             _enemyHealth.OnHealthChanged += HandleHealthChanged;
         }
-        
+
         private void OnDisable()
         {
             _enemyHealth.OnHealthChanged -= HandleHealthChanged;
         }
-        
+
         private void HandleHealthChanged(int currentHealth)
         {
             if (currentHealth <= 0)
---
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Features/Enemy/EnemyMovement.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Enemy
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class EnemyMovement : MonoBehaviour
+    {
+        public event Action<Vector3> OnDestinationReached;
+
+        public Vector3 CurrentDestination => _agent.destination;
+
+        private NavMeshAgent _agent;
+
+        private void Awake()
+        {
+            _agent = GetComponent<NavMeshAgent>();
+        }
+
+        private void FixedUpdate()
+        {
+            if (_agent.hasPath &&
+                _agent.isStopped == false &&
+                _agent.remainingDistance <= _agent.stoppingDistance)
+            {
+                OnDestinationReached?.Invoke(CurrentDestination);
+            }
+        }
+
+        public void MoveTo(Vector3 targetPosition)
+        {
+            _agent.SetDestination(targetPosition);
+        }
+
+        public void StopMoving()
+        {
+            _agent.isStopped = true;
+        }
+
+        public void ResumeMoving()
+        {
+            _agent.isStopped = false;
+        }
+    }
+}
