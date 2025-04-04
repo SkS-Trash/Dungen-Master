2b662f279ff358c7513e91b81a08367e0a29b276
ShutovKS <schutow.kir@yandex.ru>
2025-04-04 09:35:41 +0300

    Add EnemyHealth

---
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Features/Enemy/EnemyHealth.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class EnemyHealth : MonoBehaviour
+    {
+        public event Action<int> OnHealthChanged;
+
+        [field: SerializeField, ReadOnly] public int CurrentHealth { get; private set; }
+
+        [SerializeField, HideInEditorMode] private int maxHealth = 100;
+
+        private void Start()
+        {
+            CurrentHealth = maxHealth;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            CurrentHealth -= damage;
+
+            if (CurrentHealth <= 0)
+            {
+                CurrentHealth = 0;
+            }
+
+            OnHealthChanged?.Invoke(CurrentHealth);
+        }
+
+        public void Heal(int healAmount)
+        {
+            CurrentHealth += healAmount;
+            if (CurrentHealth > maxHealth)
+            {
+                CurrentHealth = maxHealth;
+            }
+
+            OnHealthChanged?.Invoke(CurrentHealth);
+        }
+    }
+}
