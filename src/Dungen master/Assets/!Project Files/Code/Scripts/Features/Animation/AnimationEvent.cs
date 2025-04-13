237e22512256b40738d8b66232c0cdcc7f980049
ShutovKS <schutow.kir@yandex.ru>
2025-04-14 02:21:29 +0300

    Add AnimationEvent

---
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Features/Animation/AnimationEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Animation
+{
+    public abstract class AnimationEvent<TEventType> : MonoBehaviour where TEventType : Enum
+    {
+        public event Action<TEventType> OnAnimationEvent;
+        
+        // Этот метод вызывается из анимационного события в аниматоре.
+        public void AnimationEventHandler(string eventName)
+        {
+            var eventType = Enum.Parse(typeof(TEventType), eventName);
+            if (eventType is TEventType eventEnum)
+            {
+                Debug.Log(eventEnum.ToString());
+                OnAnimationEvent?.Invoke(eventEnum);
+                return;
+            }
+
+            Debug.LogError($"Имя события '{eventName}' не соответствует ни одному значению " +
+                           $"перечисления типа '{typeof(TEventType)}'.");
+        }
+    }
+}
