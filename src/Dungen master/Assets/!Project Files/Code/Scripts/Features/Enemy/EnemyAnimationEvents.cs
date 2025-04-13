bead592e35577d42399aaa6ceb5a60f5fc09aebb
ShutovKS <schutow.kir@yandex.ru>
2025-04-14 02:22:16 +0300

    Add AnimationEvent and AnimatorController for enemies

---
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Features/Enemy/EnemyAnimationEvents.cs
@@ -0,0 +1,18 @@
+﻿using Animation;
+
+namespace Enemy
+{
+    public class EnemyAnimationEvents : AnimationEvent<EnemyAnimationEvents.AnimationEventType>
+    {
+        public enum AnimationEventType
+        {
+            EnablePhysicalAttack,
+            DisablePhysicalAttack,
+            LaunchMagicAttack,
+
+            GetHitEnd,
+            DeathEnd,
+            AttackEnd,
+        }
+    }
+}
--- src/Dungen master/Assets/!Project Files/Code/Scripts/Features/Enemy/EnemyAnimator.cs
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Features/Enemy/EnemyAnimator.cs
@@ -4,6 +4,18 @@ namespace Enemy
 {
     public class EnemyAnimator : MonoBehaviour
     {
+        private static readonly int Walk = Animator.StringToHash("Walk");
+        private static readonly int Run = Animator.StringToHash("Run");
+        private static readonly int Hit = Animator.StringToHash("Hit");
+        private static readonly int Attack = Animator.StringToHash("Attack");
+        private static readonly int Death = Animator.StringToHash("Death");
+
         [SerializeField] private Animator animator;
+
+        public void SetIsWalk(bool value) => animator.SetBool(Walk, value);
+        public void SetIsRun(bool value) => animator.SetBool(Run, value);
+        public void LaunchHit() => animator.SetTrigger(Hit);
+        public void LaunchAttack() => animator.SetTrigger(Attack);
+        public void LaunchDeath() => animator.SetTrigger(Death);
     }
 }
