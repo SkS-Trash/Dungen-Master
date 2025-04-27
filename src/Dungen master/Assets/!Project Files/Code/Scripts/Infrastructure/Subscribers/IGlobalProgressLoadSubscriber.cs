18060d26843cf51ef89b74d801174d9338da2f9f
ShutovKS <schutow.kir@yandex.ru>
2025-04-27 16:47:33 +0300

    Add ProgressLoadSubscribers

---
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Infrastructure/Subscribers/IGlobalProgressLoadSubscriber.cs
@@ -0,0 +1,17 @@
+﻿using Progress;
+using Subscribers.EventBusSystem;
+
+namespace Services.Progress
+{
+    /// <summary>
+    /// Интерфейс для подписчиков на загрузку глобального прогресса.
+    /// </summary>
+    public interface IGlobalProgressLoadSubscriber : IGlobalSubscriber
+    {
+        /// <summary>
+        /// Загрузить глобальный прогресс.
+        /// </summary>
+        /// <param name="progress"> Прогресс уровня.</param>
+        void OnProgressLoaded(GlobalSaveData progress);
+    }
+}
