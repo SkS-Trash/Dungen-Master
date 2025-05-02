b7fc3a8660aeb9841ae613a1ba8f023467374212
ShutovKS <schutow.kir@yandex.ru>
2025-05-02 12:36:01 +0300

    Add HomePauseState

--- src/Dungen master/Assets/!Project Files/Code/Scripts/Core/Project/Home/HomeState.cs
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Core/Project/Home/HomeState.cs
@@ -1,18 +1,41 @@
 ﻿using Cysharp.Threading.Tasks;
+using Observers.Input;
+using Services.Window;
 using StateMachines.DirectControlMultiLayer;
 
 namespace Core.Project.Home
 {
     public class HomeState : IState, IEnterable, IExitable
     {
+        private readonly IInputActionReader _inputReader;
+        private readonly IWindowService _windowService;
+
+        public HomeState(
+            IInputActionReader inputReader,
+            IWindowService windowService
+        )
+        {
+            _inputReader = inputReader;
+            _windowService = windowService;
+        }
+
         public UniTask OnEnterAsync(Unit _)
         {
+            _inputReader.OnCancelChanged += OnCancelChanged;
+
             return UniTask.CompletedTask;
         }
 
         public UniTask OnExitAsync()
         {
+            _inputReader.OnCancelChanged -= OnCancelChanged;
+
             return UniTask.CompletedTask;
+        }
+
+        private void OnCancelChanged()
+        {
+            _windowService.Open(WindowID.HomePauseMenu);
         }
     }
 }
