399b6c9f3d3080d51f96a5e53f51f4caf2e3e4f1
ShutovKS <schutow.kir@yandex.ru>
2025-05-02 13:32:23 +0300

    Add GameEventObserverCollection

---
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Features/GameEventObserver/GameEventObserverCollection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace GameEventObserver
+{
+    [CreateAssetMenu(fileName = nameof(GameEventObserverCollection),
+        menuName = "GameEventObserver/GameEventObserverCollection")]
+    public class GameEventObserverCollection : ScriptableObject
+    {
+        [field: ListDrawerSettings(IsReadOnly = true)]
+        [field: SerializeField]
+        public List<ObserverItem> Observers { get; private set; }
+
+        private void OnValidate()
+        {
+            AllEventsValidation();
+            RemoveNoneValidation();
+        }
+
+        private void AllEventsValidation()
+        {
+            var events = Enum.GetValues(typeof(GameEventType)).Cast<GameEventType>().ToArray();
+            foreach (var eventType in events)
+            {
+                if (eventType == GameEventType.None) continue;
+
+                if (Observers.All(x => x.EventType != eventType))
+                {
+                    Observers.Add(new ObserverItem { EventType = eventType });
+                }
+            }
+        }
+
+        private void RemoveNoneValidation()
+        {
+            Observers = Observers.Where(x => x.EventType != GameEventType.None).ToList();
+        }
+
+        [Serializable]
+        public class ObserverItem
+        {
+            [ReadOnly] public GameEventType EventType;
+            public GameEventObserverBehaviour Observer;
+        }
+    }
+}
