24231967fc3d55b51e419e30a0b51ad93f035a5f
ShutovKS <schutow.kir@yandex.ru>
2025-05-16 09:45:34 +0300

    Add core far new AI logic for enemy

---
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Features/Enemy/Core/EnemyStatsSO.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Enemy.Core
+{
+    [CreateAssetMenu(menuName = "AI/Enemy Stats")]
+    public class EnemyStatsSO : ScriptableObject
+    {
+        public int maxHealth = 100;
+        public float moveSpeed = 3;
+        public float aggroRadius = 10;
+        public float attackRadius = 2;
+    }
+}
---
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Features/Enemy/Core/StateController.cs
@@ -0,0 +1,70 @@
+﻿using Enemy.Components;
+using UnityEngine;
+
+namespace Enemy.Core
+{
+    [RequireComponent(typeof(EnemyMovement), typeof(EnemyAnimator), typeof(EnemyHealth))]
+    [RequireComponent(typeof(AttackCooldownTracker))]
+    [DisallowMultipleComponent]
+    public class StateController : MonoBehaviour
+    {
+        [Tooltip("Start State (SO)")] public State currentState;
+
+        [field: SerializeField] public EnemyStatsSO Stats { get; private set; }
+        [field: SerializeField] public AttackPatternSO AttackPattern { get; private set; }
+
+        public EnemyAnimator Animator { get; private set; }
+        public EnemyHealth Health { get; private set; }
+        public EnemyMovement Movement { get; private set; }
+        public Transform Player { get; private set; }
+
+
+        private void Awake()
+        {
+            Movement = GetComponent<EnemyMovement>();
+
+            Animator = GetComponent<EnemyAnimator>();
+
+            Health = GetComponent<EnemyHealth>();
+            Health.SetMaxHealth(Stats.maxHealth);
+        }
+
+        private void Update() => currentState?.UpdateState(this);
+
+        public void TransitionTo(State next)
+        {
+            if (!next || next == currentState) return;
+            currentState?.Exit(this);
+            currentState = next;
+            currentState.Enter(this);
+        }
+
+        public void SetPlayerTransform(Transform playerTransform) =>
+            Player = playerTransform;
+    }
+
+    public class Cooldown
+    {
+        private float _cooldownDuration;
+        private float _nextReadyTime;
+
+        public Cooldown(float duration)
+        {
+            _cooldownDuration = duration;
+            _nextReadyTime = -Mathf.Infinity;
+        }
+
+        public bool IsReady => Time.time >= _nextReadyTime;
+
+        public void Trigger()
+        {
+            _nextReadyTime = Time.time + _cooldownDuration;
+        }
+
+        public float NormalizedRemaining()
+        {
+            if (IsReady) return 0f;
+            return Mathf.Clamp01((_nextReadyTime - Time.time) / _cooldownDuration);
+        }
+    }
+}
