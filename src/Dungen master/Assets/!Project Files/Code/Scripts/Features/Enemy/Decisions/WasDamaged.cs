fee22b741d5d3fe5f6636ed55ad271873ccafbee
ShutovKS <schutow.kir@yandex.ru>
2025-05-16 09:46:26 +0300

    Add decisions

---
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Features/Enemy/Decisions/WasDamaged.cs
@@ -0,0 +1,11 @@
+﻿using Enemy.Core;
+using UnityEngine;
+
+namespace Enemy.Decisions
+{
+    [CreateAssetMenu(menuName = "AI/Decision/Was Damaged")]
+    public class WasDamaged : DecisionSO
+    {
+        public override bool Decide(StateController c) => c.Health.WasDamaged;
+    }
+}
