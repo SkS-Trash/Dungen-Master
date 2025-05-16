3b2d0407d6877a7ff02b798b8b128b252c93ffff
ShutovKS <schutow.kir@yandex.ru>
2025-05-16 09:46:50 +0300

    Add components

---
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Features/Enemy/Components/AttackCooldownTracker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Enemy.Core;
+using UnityEngine;
+
+namespace Enemy.Components
+{
+    public class AttackCooldownTracker : MonoBehaviour
+    {
+        private readonly Dictionary<EnemyAttackSO, float> _cooldowns = new();
+
+        public bool IsReady(EnemyAttackSO attack)
+        {
+            return !_cooldowns.TryGetValue(attack, out var readyAt) || Time.time >= readyAt;
+        }
+
+        public void SetCooldown(EnemyAttackSO attack)
+        {
+            _cooldowns[attack] = Time.time + attack.cooldown;
+        }
+
+        public void ResetCooldown()
+        {
+            var attacks = new HashSet<EnemyAttackSO>(_cooldowns.Keys);
+            foreach (var attack in attacks)
+            {
+                _cooldowns[attack] = Time.time;
+            }
+        }
+    }
+}
---
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Features/Enemy/Components/Events/EnemyAnimationEvents.cs
@@ -0,0 +1,18 @@
+﻿using Animation;
+
+namespace Enemy.Components.Events
+{
+    public class EnemyAnimationEvents : AnimationEvent<EnemyAnimationEvents.AnimationEventType>
+    {
+        public enum AnimationEventType
+        {
+            EnablePhysicalAttack,
+            DisablePhysicalAttack,
+            LaunchMagicAttack,
+
+            GetHitEnd,
+            DeathEnd,
+            AttackEnd,
+        }
+    }
+}
