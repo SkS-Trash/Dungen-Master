39e64647daa978b7ff9b3f649c8760f249211aa9
ShutovKS <schutow.kir@yandex.ru>
2025-05-16 09:47:08 +0300

    Add actions

---
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Features/Enemy/Actions/MoveToTarget.cs
@@ -0,0 +1,16 @@
+﻿using Enemy.Core;
+using UnityEngine;
+
+namespace Enemy.Actions
+{
+    [CreateAssetMenu(menuName = "AI/Action/Move To Player")]
+    public class MoveToTarget : ActionSO
+    {
+        public override void Act(StateController c)
+        {
+            c.Movement.SetSpeed(c.Stats.moveSpeed);
+            c.Movement.MoveTo(c.Player.position);
+            c.Animator.SetIsRun(true);
+        }
+    }
+}
