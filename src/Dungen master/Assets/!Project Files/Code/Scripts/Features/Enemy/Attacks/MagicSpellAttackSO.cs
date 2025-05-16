03b492179a4562c9d8d164a7e31e0278d4059627
ShutovKS <schutow.kir@yandex.ru>
2025-05-16 09:47:00 +0300

    Add attacks

---
+++ src/Dungen master/Assets/!Project Files/Code/Scripts/Features/Enemy/Attacks/MagicSpellAttackSO.cs
@@ -0,0 +1,23 @@
+﻿using Enemy.Core;
+using Magic;
+using Magic.Data;
+using UnityEngine;
+
+namespace Enemy.Attacks
+{
+    [CreateAssetMenu(menuName = "AI/Attack/Magic")]
+    public class MagicSpellAttackSO : EnemyAttackSO
+    {
+        [SerializeField] private Spell spell;
+
+        public override void Execute(StateController controller)
+        {
+            var magic = controller.GetComponent<MagicCastController>();
+            if (magic && magic.CanCast())
+            {
+                magic.SetSpell(spell);
+                controller.Animator.LaunchAttack(); // ивент вызовет Cast
+            }
+        }
+    }
+}
